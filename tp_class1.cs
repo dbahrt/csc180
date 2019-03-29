671e60838e03064b4c9d54d7369c7fd141a31334
Dan Bahrt <28609248+dbahrt@users.noreply.github.com>
2019-03-29 10:33:37 -0700

    Update tp_class1.cs

--- tp_class1.cs
+++ tp_class1.cs
@@ -9,35 +9,51 @@
 using System;
 
 namespace BoxApplication {
-   class Box {
-      public double length;   // Length of a box
-      public double breadth;  // Breadth of a box
-      public double height;   // Height of a box
-   }
-   class Boxtester {
-      static void Main(string[] args) {
-         Box Box1 = new Box();   // Declare Box1 of type Box
-         Box Box2 = new Box();   // Declare Box2 of type Box
-         double volume = 0.0;    // Store the volume of a box here
+    //---------
+    // declared a class... a pattern with which objects can be created
+    //---------
+    class Box {
+        // a class can contain data items (fields and properties)
+        // as well as methods (functions) that operate on that data
 
-         // box 1 specification
-         Box1.height = 5.0;
-         Box1.length = 6.0;
-         Box1.breadth = 7.0;
+        public double length;   // Length of a box
+        public double breadth;  // Breadth of a box
+        public double height;   // Height of a box
+    }
 
-         // box 2 specification
-         Box2.height = 10.0;
-         Box2.length = 12.0;
-         Box2.breadth = 13.0;
+    //----------
+    // startup class... contains Main() static function
+    //----------
+    class Boxtester {
+
+        //----------
+        static void Main(string[] args) {
+            // this is a temporary local variable that will hold a
+            // double-precision floating point number
+            double volume = 0.0;    // Store the volume of a box here
+
+            // instantiate box 1 & initialize it
+            Box Box1 = new Box();   // instantiate mean make a copy or instance
+            Box1.height = 5.0;
+            Box1.length = 6.0;
+            Box1.breadth = 7.0;
+
+            // instantiate box 2 & initialize it
+            Box Box2 = new Box();   // make another object
+            Box2.height = 10.0;
+            Box2.length = 12.0;
+            Box2.breadth = 13.0;
            
-         // volume of box 1
-         volume = Box1.height * Box1.length * Box1.breadth;
-         Console.WriteLine("Volume of Box1 : {0}",  volume);
+            // calculate volume of box 1
+            volume = Box1.height * Box1.length * Box1.breadth;
+            Console.WriteLine("Volume of Box1 : {0}",  volume);
 
-         // volume of box 2
-         volume = Box2.height * Box2.length * Box2.breadth;
-         Console.WriteLine("Volume of Box2 : {0}", volume);
-         Console.ReadKey();
-      }
-   }
+            // calculate volume of box 2
+            volume = Box2.height * Box2.length * Box2.breadth;
+            Console.WriteLine("Volume of Box2 : {0}", volume);
+
+            Console.Write("Press Enter to continue...");
+            Console.ReadKey();
+        }
+    }
 }
