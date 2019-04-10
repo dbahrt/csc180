a2d7596c3b0a6e3724675dad9d55806b7b474f26
Dan Bahrt <28609248+dbahrt@users.noreply.github.com>
2019-04-10 12:28:47 -0700

    factor tryTryParse() out of readStringsFromKeyboard()

--- sample_data_types.cs
+++ sample_data_types.cs
@@ -1,7 +1,7 @@
 // author: Dan Bahrt
 // sample_data_types.cs (coding_patterns/hello_readStringsFromKeyboard.cs)
-// last updated: 8 Apr 2019
-
+// last updated: 10 Apr 2019
+// command line: sample_data_types.cs < sample.dat > output.txt
 
 using System;
 using System.Collections.Generic;
@@ -51,90 +51,95 @@ public class Startup {
         result=new List<string>();
 
         for(;;) {
-            string inpstr="";
-                inpstr=Console.ReadLine();
-                if(inpstr==null) {
-                    Console.WriteLine("inpstr is null");
-                    break;
-                }
-                if(inpstr.Trim()=="") {
-                    Console.WriteLine("EOF detected");
-                    break;
-                }
+            string inpstr=Console.ReadLine();
+            if(inpstr==null) {
+                Console.WriteLine("inpstr is null");
+                break;
+            }
+            if(inpstr.Trim()=="") {
+                Console.WriteLine("EOF detected");
+                break;
+            }
 
-                result.Add(inpstr);
+            result.Add(inpstr);
 
-                tryTypeConversions(inpstr);
+            tryTryParseConversions(inpstr);
         }
-
-Console.WriteLine(inpstr);
-            Byte outByte;
-            if(Byte.TryParse(inpstr,out outByte)) {
-                Console.WriteLine("    Byte\t"+outByte);
-            }
-
-            SByte outSByte;
-            if(SByte.TryParse(inpstr,out outSByte)) {
-                Console.WriteLine("    SByte\t"+outSByte);
-            }
-
-            Int16 outInt16;
-            if(Int16.TryParse(inpstr,out outInt16)) {
-                Console.WriteLine("    Int16\t"+outInt16);
-            }
-
-            UInt16 outUInt16;
-            if(UInt16.TryParse(inpstr,out outUInt16)) {
-                Console.WriteLine("    UInt16\t"+outUInt16);
-            }
-
-            Int32 outInt32;
-            if(Int32.TryParse(inpstr,out outInt32)) {
-                Console.WriteLine("    Int32\t"+outInt32);
-            }
-
-            UInt32 outUInt32;
-            if(UInt32.TryParse(inpstr,out outUInt32)) {
-                Console.WriteLine("    UInt32\t"+outUInt32);
-            }
-
-            Int64 outInt64;
-            if(Int64.TryParse(inpstr,out outInt64)) {
-                Console.WriteLine("    Int64\t"+outInt64);
-            }
-
-            UInt64 outUInt64;
-            if(UInt64.TryParse(inpstr,out outUInt64)) {
-                Console.WriteLine("    UInt64\t"+outUInt64);
-            }
-
-            Single outSingle;
-            if(Single.TryParse(inpstr,out outSingle)) {
-                Console.WriteLine("    Single\t"+outSingle);
-            }
-
-            Double outDouble;
-            if(Double.TryParse(inpstr,out outDouble)) {
-                Console.WriteLine("    Double\t"+outDouble);
-            }
-
-            Decimal outDecimal;
-            if(Decimal.TryParse(inpstr,out outDecimal)) {
-                Console.WriteLine("    Decimal\t"+outDecimal);
-            }
-           
-            Boolean outBoolean;
-            if(Boolean.TryParse(inpstr,out outBoolean)) {
-                Console.WriteLine("    Boolean\t"+outBoolean);
-            }
-           
-            DateTime outDateTime;
-            if(DateTime.TryParse(inpstr,out outDateTime)) {
-                Console.WriteLine("    DateTime\t"+outDateTime);
-            }
            
         return true;
     } // end function readStringsFromKeyboard()
+
+    //----------
+    private static void tryTryParseConversions(string inpstr) {
+        Console.WriteLine(inpstr);
+        
+        Byte outByte;
+        if(Byte.TryParse(inpstr,out outByte)) {
+            Console.WriteLine("    Byte\t"+outByte);
+        }
+
+        SByte outSByte;
+        if(SByte.TryParse(inpstr,out outSByte)) {
+            Console.WriteLine("    SByte\t"+outSByte);
+        }
+
+        Int16 outInt16;
+        if(Int16.TryParse(inpstr,out outInt16)) {
+            Console.WriteLine("    Int16\t"+outInt16);
+        }
+
+        UInt16 outUInt16;
+        if(UInt16.TryParse(inpstr,out outUInt16)) {
+            Console.WriteLine("    UInt16\t"+outUInt16);
+        }
+
+        Int32 outInt32;
+        if(Int32.TryParse(inpstr,out outInt32)) {
+            Console.WriteLine("    Int32\t"+outInt32);
+        }
+
+        UInt32 outUInt32;
+        if(UInt32.TryParse(inpstr,out outUInt32)) {
+            Console.WriteLine("    UInt32\t"+outUInt32);
+        }
+
+        Int64 outInt64;
+        if(Int64.TryParse(inpstr,out outInt64)) {
+            Console.WriteLine("    Int64\t"+outInt64);
+        }
+
+        UInt64 outUInt64;
+        if(UInt64.TryParse(inpstr,out outUInt64)) {
+            Console.WriteLine("    UInt64\t"+outUInt64);
+        }
+
+        Single outSingle;
+        if(Single.TryParse(inpstr,out outSingle)) {
+            Console.WriteLine("    Single\t"+outSingle);
+        }
+
+        Double outDouble;
+        if(Double.TryParse(inpstr,out outDouble)) {
+            Console.WriteLine("    Double\t"+outDouble);
+        }
+
+        Decimal outDecimal;
+        if(Decimal.TryParse(inpstr,out outDecimal)) {
+            Console.WriteLine("    Decimal\t"+outDecimal);
+        }
+          
+        Boolean outBoolean;
+        if(Boolean.TryParse(inpstr,out outBoolean)) {
+            Console.WriteLine("    Boolean\t"+outBoolean);
+        }
+           
+        DateTime outDateTime;
+        if(DateTime.TryParse(inpstr,out outDateTime)) {
+            Console.WriteLine("    DateTime\t"+outDateTime);
+        }
+           
+        return;
+    } // end function tryTryParseConversions()
 
 } // end class Startup
     
